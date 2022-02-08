6eddc9ea2b4233d00a7b048d921691af9e0eca1b
rakibshakib <rakibshakib74@gmail.com>
2022-02-08 19:39:21 +0600

    add isexisting product condition in to cart

--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,10 +1,15 @@
 export const reducer = (state, action) => {
   switch (action.type) {
     case "ADD_TO_CART":
-      return {
-        ...state,
-        addedToCart: [...state.addedToCart, action.payload],
-      };
+      const isAlreadyHave = state.addedToCart.find(
+        (item) => item.id === action.payload?.id
+      );
+      if (!isAlreadyHave) {
+        return {
+          ...state,
+          addedToCart: [...state.addedToCart, action.payload],
+        };
+      }
     case "REMOVE_FROM_CART":
       const remainnigProduct = state.addedToCart.filter(
         (item) => item.id !== action.payload
